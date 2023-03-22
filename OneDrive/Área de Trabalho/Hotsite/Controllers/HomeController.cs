f940c84e991b5a0f3b42c4c5ffa829fa3699d501
Nicole Velloso <nicole.velloso28@gmail.com>
2023-03-22 16:40:53 -0300

    Criação de projeto

---
+++ OneDrive/Área de Trabalho/Hotsite/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Hotsite.Models;
+
+namespace Hotsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Interesse cad)
+        {
+        
+
+       try
+       {
+         DatabaseService dbs = new DatabaseService();
+           dbs.CadastraInteresse(cad);
+            return View("Index",cad);
+    
+    }catch(Exception ex) {
+     Console.WriteLine(ex);
+        return View("Index");
+    }
+}
+}
+}
+ 
